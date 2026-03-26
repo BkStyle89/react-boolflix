6e11b39120609dc402a1fbed04e9615c3695946a
BkStyle89 <kajzer@hotmail.it>
2026-03-26 12:08:42 +0100

    search films

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import { useState } from "react";
 function App() {
 
   const api_key=import.meta.env.VITE_API_KEY
   
-  const api_url=`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+fut`
-  console.log(api_url);
+  const api_url=`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=ritorno+al+futuro`
   
+  const [searchFilm, setSearchFilm]=("")
+  const [films,setFilms]=useState([])
+
+  function findFilm(){
+  fetch(api_url)
+  .then(res=>res.json())
+  .then(data=>setFilms(data)
+  
+  
+)}
+
+
+
 
   return (
     <>
@@ -14,9 +26,10 @@ function App() {
           <input id="search-focus" type="search" className="form-control" />
           <label className="form-label">Search</label>
         </div>
-        <button type="button" className="btn btn-primary" data-mdb-ripple-init>
+        <button onClick={searchFilm} type="button" className="btn btn-primary" data-mdb-ripple-init>
         <i className="fas fa-search"> cerca</i>
         </button>
+
       </div>
     </>
   )
