dc1f47f1b9f2fa435b95b1840aa4146a8f8b6368
BkStyle89 <kajzer@hotmail.it>
2026-03-26 16:24:22 +0100

    some changes on the button and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,44 @@ function App() {
 
   const api_key=import.meta.env.VITE_API_KEY
   
-  const api_url=`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${onSearch}`
+  const[search,setSearch] = useState("")
+  const [searchFilm,setSearchFilm] = useState("")
+  const [films,setFilms] = useState([])
+  const api_url=`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search}`
   
-  const [searchFilm, setSearchFilm]=("")
-  const [films,setFilms]=useState([])
 
   function findFilm(){
   fetch(api_url)
   .then(res=>res.json())
-  .then(data=>setFilms(data)
-  
-  
-)}
-useEffect(findFilm)
+  .then(data=>{
+     console.log(data); 
+    
+    setFilms(data.results)
+  }
 
 
-function onSearch(){
-  if(films.find(searchFilm == findFilm.title)){
-    setFilms(onSearch)
-    console.log(setFilms);
+  )}
+  useEffect(()=>{
+    findFilm()
+  },[search])
+
+
+  function onSearch(){
+    setSearch(films.find(e => e.original_title.includes(searchFilm)))
+    console.log(search);
+    
+    /*   setFilms()
+      console.log(setFilms); */
+      
     
   }
-}
 
+
+
+
+function handleSubmit(e){
+  e.preventDefault()
+}
 
 
 
@@ -33,8 +48,10 @@ function onSearch(){
     <>
       <div className="input-group">
         <div className="form-outline" data-mdb-input-init>
-          <input id="search-focus" type="search" value={searchFilm} onChange={e=> setSearchFilm(e.target.value)} className="form-control" />
+          <form onSubmit={handleSubmit}>
+          <input id="search-focus" type="text" value={searchFilm} onChange={e=> setSearchFilm(e.target.value)} className="form-control" />
           <label className="form-label">Search</label>
+          </form>
         </div>
         <button onClick={onSearch} type="button" className="btn btn-primary" data-mdb-ripple-init>
         <i className="fas fa-search"> cerca</i>
