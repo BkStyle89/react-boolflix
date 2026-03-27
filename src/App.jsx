2be709d6938efd4d1e859853e419f51dfe4c8873
BkStyle89 <kajzer@hotmail.it>
2026-03-27 12:25:55 +0100

    tv series name fix

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
     fetch(api_tv_series)
     .then(res=>res.json())
     .then(data=>{
+      console.log(data);
+      
       setTvSeries(data.results)
     })}
     useEffect(()=>{
@@ -50,49 +52,82 @@ function handleSubmit(e){
 
   return (
     <>
-    <span className="fi fi-us"></span> United States
-      <div className="input-group">
-        <div className="form-outline" data-mdb-input-init>
-          <form onSubmit={handleSubmit}>
-            <input id="search-focus" type="text" value={searchFilm} onChange={e=> setSearchFilm(e.target.value)} className="form-control" />
-            <label className="form-label">Search</label>
-          </form>
+    <div className="container">
+      <div className="row">
+          <div className="input-group">
+            <div className="form-outline" data-mdb-input-init>
+              <form onSubmit={handleSubmit}>
+                <input id="search-focus" type="text" value={searchFilm} onChange={e=> setSearchFilm(e.target.value)} className="form-control" />
+                <label className="form-label">Search</label>
+              </form>
+            </div>
+            </div>
+            <button onClick={onSearch} type="button" className="btn btn-primary" data-mdb-ripple-init>
+              <i className="fas fa-search"> cerca</i>
+            </button>
+          <div className="container ">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+              <div className="card bg-danger">
+                { films.map(film=>(
+                  <ul className="bg-light">
+                    <li key={film.id} className="bg-light">
+                      <div className="text-center bg-light">{film.title}</div>
+                      <br />
+                      <div className="text-center bg-light">{film.original_title}</div>
+                      <br />
+                      <img className="poster bg-light" src={`https://image.tmdb.org/t/p/w400/${film.poster_path}`} alt="Poster" />
+                      <br />
+                      <div className="text-center bg-light">
+                        <div className={`fi fi-${film.original_language}`}></ div>
+                      </div>
+                      <br />
+                      <div className="d-flex justify-content-between bg-light">
+                        <div className="bg-light">
+                          {film.vote_average.toFixed()}
+                        </div>
+                        <div className="bg-light">
+                          {film.vote_count}
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+              <div className="card gy-5 bg-danger">
+                { tvSeries.map(tv=>(
+                  <ul className="bg-light">
+                    <li className="bg-light" key={tv.id}>
+                      <div className="text-center bg-light">{tv.name}</div>
+                      <br />
+                      <div className="text-center bg-light">{tv.original_name
+}</div>
+                      <br />
+                      <img className="poster bg-light" src={`https://image.tmdb.org/t/p/w400/${tv.poster_path}`} alt="Poster" />
+                      <br />
+                      <div className="text-center bg-light">
+                        <div className={`fi fi-${tv.original_language}`}></div>
+                      </div>
+                      <br />
+                      <div className="d-flex justify-content-between bg-light">
+                        <div className="bg-light">
+                          {tv.vote_average}
+                        </div>
+                        <div className="bg-light">
+                          {tv.vote_count}
+                        </div>  
+                      </div>
+                    </li>
+                  </ul>
+                )) }
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        <button onClick={onSearch} type="button" className="btn btn-primary" data-mdb-ripple-init>
-          <i className="fas fa-search"> cerca</i>
-        </button>
-        { films.map(film=>(
-          <ul >
-            <li key={film.id}>
-              {film.title}
-              <br />
-              {film.original_title}
-              <br />
-              <img src={`https://image.tmdb.org/t/p/w400/${film.poster_path}`} alt="Poster" />
-              <div className={`fi fi-${film.original_language}`}></div>
-              <br />
-              {film.vote_average}
-              {film.vote_count}
-            </li>
-          </ul>
-        )) }
-        { tvSeries.map(tv=>(
-          <ul >
-            <li key={tv.id}>
-              {tv.title}
-              <br />
-              {tv.original_title}
-              <br />
-              <img src={`https://image.tmdb.org/t/p/w400/${tv.poster_path}`} alt="Poster" />
-              <div className={`fi fi-${tv.original_language}`}></div>
-              <br />
-              {tv.vote_average}
-              {tv.vote_count}
-            </li>
-          </ul>
-        )) }
-      
+      </div>
     </>
   )
 }
